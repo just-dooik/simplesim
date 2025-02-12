55c364a8ec40b847d79637d3cb43ba028e721dba
김두익 <dooikkim1005@hanyang.ac.kr>
2025-02-12 17:51:05 +0900

    - mshr.c   - implemented basic mshr functions     - mshr_create     - mshr_lookup     - mshr_insert     - mshr_send_request     - mshr_complete_request     - mshr_free_entry

--- simplesim-3.0/mshr.c
+++ simplesim-3.0/mshr.c
@@ -1,22 +1,207 @@
-/* */
+/* MSHR functions implementation */
 
 #include "mshr.h"   
+#include "memory.h" /* for enum mem_cmd */
+#include "sim-outorder.c" // Add this include for RUU_station definition
+#include "machine.h" // Add this include for enum md_opcode
+
+/*
+* MSHR macros
+* simplescalar simulator goals to be faster, so we define some macros to speed up the code
+* using bitwise operations
+*/
+/* get the offset of the block */
+#define MSHR_BLK_OFFSET(mshr, addr) \
+  ((addr) & (mshr)->blk_mask)
+/* get the address of the block */
+#define MSHR_BLK_ADDR(mshr, addr) \
+  ((addr) & ~(mshr)->blk_mask)
+/* check if the mshr is full */
+#define MSHR_IS_FULL(mshr) \
+  ((mshr)->nvalid == (mshr)->nentries)  
+/* check if the entry is full */
+#define MSHR_ENTRY_IS_FULL(mshr, entry) \
+  ((entry)->status == MSHR_ENTRY_FULL)
+/* check if the entry is valid */
+#define MSHR_ENTRY_IS_VALID(mshr, entry) \
+  ((entry)->status & MSHR_ENTRY_VALID)
 
 
 /* create mshr */
 struct mshr_t *
-mshr_create(int size)
+mshr_create(
+  int bsize, /* block size */
+  int nentries, /* number of entries*/
+  int nblks, /* number of blocks for each entry*/
+  mshr_mem_access_fn mem_access_fn
+)
 {
   struct mshr_t *mshr;
-  
-  /* TODO: Check parameters (but do we actually need it?)*/ 
 
   mshr = (struct mshr_t *)
-    calloc(1, sizeof(struct mshr_t));
+    calloc(1, sizeof(struct mshr_t) + (nentries-1) * sizeof(struct mshr_entry_t));
   if(!mshr)
     fatal("out of virtual memory");
 
+  mshr->nentries = nentries;
+  mshr->nblks = nblks;
+  mshr->bsize = bsize;
+
+  /* set the block mask and shift same as cache for efficient decoding */
+  mshr->blk_mask = (1 << bsize) - 1;
+  mshr->blk_shift = (int)log2(mshr->blk_mask);
+
+
+  /* allocate entries */
+  mshr->entries = (struct mshr_entry_t *)
+    calloc(nentries, sizeof(struct mshr_entry_t));
+  if(!mshr->entries)
+    fatal("out of virtual memory");
+
+  /* allocate blocks */
+  for(int i = 0; i < nentries; i++) {
+    mshr->entries[i].blk = (struct mshr_blk_t *)
+      calloc(nblks, sizeof(struct mshr_blk_t));
+    if(!mshr->entries[i].blk)
+      fatal("out of virtual memory");
+  }
+
+  /* 메모리 접근 함수 설정 */
+  mshr->mem_access_fn = mem_access_fn;
+
+  return mshr;
+
+  /* initialize entries */
+  for(int i = 0; i < nentries; i++) {
+    mshr->entries[i].status = 0; 
+    mshr->entries[i].block_addr = 0;
+  } 
+
+  /* initialize blocks */
+  for(int i = 0; i < nentries; i++) {
+    for(int j = 0; j < nblks; j++) {
+      mshr->entries[i].blk[j].status = 0;
+      mshr->entries[i].blk[j].offset = 0;
+      mshr->entries[i].blk[j].dest = NULL;
+    }
+  } 
+
+  return mshr;
+}
+
+/* lookup mshr
+ * returns valid entry if found, otherwise returns NULL
+ */
+
+struct mshr_entry_t *
+mshr_lookup(
+  struct mshr_t *mshr, 
+  md_addr_t addr /* address */
+)
+{ 
+  md_addr_t block_addr = MSHR_BLOCK_ADDR(mshr, addr);
+
+  /* check if the entry is valid */
+  for(int i = 0; i < mshr->nentries; i++) { 
+    if((mshr->entries[i].status & MSHR_ENTRY_VALID) && mshr->entries[i].block_addr == block_addr) {
+      return &mshr->entries[i];
+    }
+  }
+  return NULL;
+}
+
+/* mshr insert */ // TODO: sent인 경우 처리 필요
+struct mshr_entry_t *
+mshr_insert(
+  struct mshr_t *mshr,
+  md_addr_t addr,
+  struct RUU_station *rs,
+  tick_t now
+)
+{
+  struct mshr_entry_t *entry;
+  struct mshr_blk_t *blk;
+
+  entry = mshr_lookup(mshr, addr);
+  if (!entry) {
+    /* 새 entry 할당 */
+    entry = get_free_entry(mshr);
+    if (!entry) return NULL;
+    
+    /* 메모리 요청 전송 */
+    unsigned int lat = mshr_send_request(mshr, entry, now);
+  }
   
+  /* 블록 추가 */
+  if(entry && entry->nvalid < mshr->nblks) { // if entry is valid and not full
+    blk = &entry->blk[entry->nvalid++];
+    blk->status &= ~MSHR_BLOCK_VALID;
+    blk->offset = MSHR_BLK_OFFSET(mshr, addr);
+    blk->dest = rs;
+    if(MSHR_ENTRY_IS_FULL(mshr, entry)) { // entry is full 
+      entry->status |= MSHR_ENTRY_FULL;
+    }
+  }
+
+  return entry; // return valid entry, if not valid, return NULL
+}
+
+/* free entry 
+ * flushing entry due to misspeculation
+*/
+void
+mshr_free_entry(
+  struct mshr_t *mshr, 
+  struct mshr_entry_t *entry
+)
+{
+  for(int i = 0; i < entry->nvalid; i++) {
+    struct mshr_blk_t *blk = &entry->blk[i];
+    blk->status &= ~MSHR_BLOCK_VALID; // clear the valid status
+  }
+  entry->nvalid = 0; // clear the number of valid blocks  
+  entry->status &= ~MSHR_ENTRY_VALID; // clear the valid status
+}
 
 
+/* memory request send function */
+unsigned int 
+mshr_send_request(
+  struct mshr_t *mshr, 
+  struct mshr_entry_t *entry, 
+  tick_t now
+)
+{
+  unsigned int lat;
+
+  lat = mshr->mem_access_fn(Read, entry->block_addr, mshr->bsize, entry, now);
+
+  entry->status |= MSHR_ENTRY_PENDING;
+  return lat;
+}
+
+/* memory request complete function 
+* complete the memory request and update the entry status 
+* send the data to the destined LSQ station
+*/
+void 
+mshr_complete_reqeust(
+  struct mshr_t *mshr, 
+  struct mshr_entry_t *entry, 
+  byte_t *data, 
+  tick_t now
+)
+{
+
+  for(int i = 0; i < entry->nvalid; i++) {
+    struct mshr_blk_t *blk = &entry->blk[i];
+    if(blk->status & MSHR_BLOCK_VALID) {
+      struct RUU_station *dest = blk->dest;
+      dest->completed = TRUE; // set the completed status of the destination station
+      blk->status &= ~MSHR_BLOCK_VALID; // clear the valid status of the block
+    }
+  }
+
+  entry->nvalid = 0; // clear the number of valid blocks
+  entry->status &= ~MSHR_ENTRY_VALID; // clear the valid status
 }
--- simplesim-3.0/mshr.h
+++ simplesim-3.0/mshr.h
@@ -6,38 +6,110 @@
 #define MSHR_H
 
 #include <stdio.h>
-
 #include "machine.h"
-
-#define MSHR_BLOCK_VALID   0x00000001 /* MSHR block is valid, in use*/
-#define MSHR_BLOCK_DIRTY   0x00000002 /* MSHR block is dirty */
-
-#define MSHR_ENTRY_VALID   0x00000001 /* MSHR entry is valid, in use*/
-#define MSHR_ENTRY_DIRTY   0x00000002 /* MSHR entry is dirty */
-#define MSHR_ENTRY_TYPE_INST 0x00000004 /* MSHR entry is instruction */ 
-#define MSHR_ENTRY_TYPE_DATA 0x00000008 /* MSHR entry is data */  
+#include "memory.h"
 
 
+/* forward declaration of RUU station */
+struct RUU_station;
+
+/* MSHR Block Status */
+#define MSHR_BLOCK_VALID    0x00000001  /* 사용 중 */
+#define MSHR_BLOCK_READY    0x00000002  /* 데이터 도착, 처리 대기 */
+
+/* MSHR Entry Status */
+#define MSHR_ENTRY_VALID    0x00000001  /* 사용 중 */
+#define MSHR_ENTRY_FULL     0x00000002  /* 블록 가득 참 */
+#define MSHR_ENTRY_PENDING  0x00000004  /* 메모리 접근 중 */
+#define MSHR_ENTRY_PREFETCH 0x00000008  /* 프리페치 요청 */
+
+/* MSHR block structure */
 struct mshr_blk_t {
-  struct mshr_blk_t *next; /* next MSHR block */
-  struct mshr_blk_t *prev; /* previous MSHR block */
-  struct RUU_STATION *dest; /* destination */
-
   unsigned int status; /* MSHR block status(valid, dirty) */
   md_addr_t offset; /* offset */  
+  struct RUU_station *dest; /* destination, only pointer is stored so forward declaration is used */ 
+  enum md_opcode op; /* opcode */ 
 };
 
+/* MSHR entry structure */
 struct mshr_entry_t {
   struct mshr_blk_t *blk; /* mshr block pointer */
   unsigned int status; /* mshr entry status(valid, dirty)  */
   md_addr_t block_addr; /* block address */
+  int nvalid; /* number of valid blocks */
 };
 
+/* memory access function type, like cache's blk_access_fn */
+typedef unsigned int
+(*mshr_mem_access_fn)(enum mem_cmd cmd,     /* Read or Write */
+                      md_addr_t addr,        /* block address */
+                      int bsize,             /* block size */
+                      struct mshr_entry_t *entry,
+                      tick_t now);           /* current time */
+
+/* MSHR structure */
 struct mshr_t {
   struct mshr_entry_t *entries; /* MSHR entries */
-
+  int nentries; /* number of entries */ 
+  int nblks; /* number of blocks for each entry */
+  int nvalid; /* number of valid entries */
+  int bsize; /* block size */ 
+  
+  /* derived data, for fast decoding */
   md_addr_t blk_mask; /* block mask */
   int blk_shift; /* block shift */
+
+  /* memory access function */
+  mshr_mem_access_fn mem_access_fn;  /* L2/memory access function */
+  unsigned int mem_lat;              /* memory access latency */
 };
+
+/* create mshr*/
+struct mshr_t *
+mshr_create(
+  int bsize, /* block size */
+  int nentries, /* number of entries*/
+  int nblks, /* number of blocks for each entry*/
+  mshr_mem_access_fn mem_access_fn
+);
+
+/* lookup mshr */
+struct mshr_entry_t *
+mshr_lookup(
+  struct mshr_t *mshr, 
+  md_addr_t addr
+);
+
+/* mshr insert */
+struct mshr_entry_t *
+mshr_insert(  
+  struct mshr_t *mshr,
+  md_addr_t addr,
+  struct RUU_station *dest,
+  tick_t now
+);
+
+/* memory request send function */
+unsigned int 
+mshr_send_request(
+  struct mshr_t *mshr, 
+  struct mshr_entry_t *entry, 
+  tick_t now
+);
+
+/* memory request complete function */
+void
+mshr_complete_request(
+  struct mshr_t *mshr, 
+  struct mshr_entry_t *entry, 
+  byte_t *data, 
+  tick_t now
+);
+
+void 
+mshr_free_entry( 
+  struct mshr_t *mshr, 
+  struct mshr_entry_t *entry
+);
 
 #endif // !MSHR_H
